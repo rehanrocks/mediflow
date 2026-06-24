2502586c4b659c89f6314e7d022b1455d5ad990d
Muhammad Husnain <husnainbhinder682@gmail.com>
2026-06-24 23:22:36 +0500

    Remove backend from frontend branch

--- backend/staff/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import StaffMemberViewSet
-
-router = DefaultRouter()
-router.register(r'staff', StaffMemberViewSet, basename='staff')
-urlpatterns = router.urls
